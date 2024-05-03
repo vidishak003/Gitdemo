75da0b19415269288d71a1fb44767b85afa5a25d
vidishak003 <vidishak99@gmail.com>
2024-05-03 19:09:34 +0530

    first commit

---
+++ src/ForgotPassword.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ForgotPassword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vidisha\\Downloads\\chromedriver-win64.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/forgot_password");
+		driver.findElement(By.cssSelector(".email")).sendKeys("vidisha@gmail.com");
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+		System.out.print(driver.findElement(By.xpath("//body/h1")).getText());		
+		driver.close();
+
+	}
+
+}
